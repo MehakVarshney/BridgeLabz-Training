f4dccb7d25321c79fc0d19c152f6072e3079d9fb
MehakVarhney <mehakvarshney1320@gmail.com>
2025-12-30 20:22:59 +0530

    [MehakVarshney] Add . new file

--- core-java-practice/scenario-based/EmployeeWages.java
+++ core-java-practice/scenario-based/EmployeeWages.java
@@ -2,14 +2,10 @@ import java.util.Random;
 public class EmployeeWages {
 
     public static void main(String[] args) {
-
         System.out.println("Welcome to Employee Wage Computation");
-
         Random random = new Random();
         int attendance = random.nextInt(2);
-
         if (attendance == 1) {
-
             System.out.println("Employee is Present");
 
             int wagePerHour = 20;
